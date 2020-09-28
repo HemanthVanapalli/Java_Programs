efd1b5de0c0af770ba7ab258d4f26fa302378aa1
Hemanth.Vanapalli <Hemanth.Vanapalli@ril.com>
2020-09-28 13:51:06 +0530

    Adding recursive methods to fibonicci and factorial

--- Practice/src/main/java/Factorial.java
+++ Practice/src/main/java/Factorial.java
@@ -2,9 +2,11 @@ public class Factorial {
 
     public static void main(String[] args) {
         getFact(5);
+        System.out.println(fact(5));
 
     }
 
+    // 1. Genral Solution
     public static void getFact(int num) {
         // !5 = 5*4*3*2*1 = 120
         //!0 = 1
@@ -14,6 +16,15 @@ public class Factorial {
             fact = fact * i;
         }
         System.out.println("Fact is " + fact);
+    }
+
+    // 2. Recursion
+    public static int fact(int n) {
+
+        if (n == 1)
+            return n;
+        else
+            return n * fact(n - 1);
     }
 
 
--- Practice/src/main/java/FibonicciSeries.java
+++ Practice/src/main/java/FibonicciSeries.java
@@ -2,9 +2,10 @@ public class FibonicciSeries {
 
     public static void main(String[] args) {
 
-        // 0 1 1 2 3 5 8 13 21
 
-        int num = 10;
+    }
+    //1. General Prgram
+    public static void fib(int num) {
         int n1 = 0;
         int n2 = 1;
         int temp = 0;
@@ -14,9 +15,26 @@ public class FibonicciSeries {
             n2 = n1;
             n1 = temp;
             System.out.println("fibonicci series " + temp);
-
-
         }
 
     }
+
+    // Recursive function to generate fibonicci sequence
+    public static int fibonicci(int n){
+        if (n <= 1)
+            return n;
+        else
+            return fibonicci(n - 1) + fibonicci(n - 2);
+    }
+
+
+    static void printReversedFib( int x) {
+        if (x <= 1)
+            System.out.println(fibonicci(x));
+        else {
+            System.out.println(fibonicci(x));
+            printReversedFib(x - 1);
+        }
+    }
 }
+
